9df548b7608da5b01be3ae28bbb1549012f325ac
Poottichai-Poom <poottichai05012549@gmail.com>
2025-10-15 14:49:20 +0700

    test6

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import authRouter from './routes/authRouter';
 import tasksRouter from './routes/tasksRouter';
 import currencyRouter from './routes/currencyRouter';
+import sumRouter from './routes/sumRouter';
 
 
 const app = express();
 app.use(express.json());
 
+app.use('/api/sum', sumRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/tasks', tasksRouter);
 app.use('/api/currency', currencyRouter);
