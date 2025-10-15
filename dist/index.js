5b4d3c8a2374f9fdf7f82024abb3cdebf2f7e565
Poottichai-Poom <poottichai05012549@gmail.com>
2025-10-15 20:37:00 +0700

    test2

--- dist/index.js
+++ dist/index.js
@@ -5,14 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const user_1 = __importDefault(require("./routes/user"));
+const auth_1 = __importDefault(require("./routes/auth"));
+const tasks_1 = __importDefault(require("./routes/tasks"));
+const currency_1 = __importDefault(require("./routes/currency"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use('/api/users', user_1.default);
-const PORT = process.env.PORT || 3000;
-// 🚨 อย่ารัน listen() ถ้าเป็น test mode
+// Route mapping
+app.use('/auth', auth_1.default);
+app.use('/tasks', tasks_1.default);
+app.use('/currency', currency_1.default);
+// 🚨 ไม่ run listen() ถ้า NODE_ENV=test
 if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(3000, () => console.log('Server running on port 3000'));
 }
 exports.default = app;
